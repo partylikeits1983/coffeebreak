b24499f539ad497591db9615886eee3dfaaa0c9f
partylikeits1983 <aleqvids@gmail.com>
2021-11-10 13:46:46 +0300

    added script

---
+++ MGIMO.py
@@ -0,0 +1,25 @@
+
+location = "home"
+sleeping = False
+
+
+
+def code():
+    print("code")
+
+def think():
+    print("think")
+
+def sleep():
+    print("sleep")
+
+for i in range (0, 365):
+    if (location != "mgimo") & (sleeping != True):
+        code()
+    elif (location == "mgimo"):
+        think()
+    else:
+        sleeping = True
+        sleep()
+
+    
